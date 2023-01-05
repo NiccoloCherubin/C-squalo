f709aca052769284500a08f46596501ed3107d5f
Niccolò Cherubin <103661034+NiccoloCehrubin@users.noreply.github.com>
2023-01-05 07:25:08 -0800

    Add files via upload

---
+++ Es2pag129/Es2pag129/Divisibile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es2pag129
+{
+    internal class Divisibile
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Inserire un numero per scoprire se è divisibile per cinque");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero % 5 == 0){
+                Console.WriteLine("{0} è divisibile per 5", numero);
+            }
+            else
+            {
+                Console.WriteLine("{0} non è divisibile per 5", numero);
+            }
+            Console.WriteLine("Premere un tasto per continuare");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ numPos/numPos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numPos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Inserire numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0) {
+                Console.WriteLine("Il numero è positivo");
+            }
+            if (num < 0)
+            {
+                Console.WriteLine("Il numero è negativo");
+            }
+            else
+            {
+                Console.WriteLine("0 non è nè positivo nè negativo");
+            }
+        }
+    }
+}
