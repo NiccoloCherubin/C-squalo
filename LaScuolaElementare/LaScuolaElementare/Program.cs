e1d7b61be7f76c5a8c2f776b2e98c8c7552beab4
Niccolò Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-12 15:27:29 +0100

    finito esercizio della scuola elementare

---
+++ LaScuolaElementare/LaScuolaElementare/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaScuolaElementare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunni = new string[3];
+            for(int i = 0; i < alunni.Length; i++)
+            {
+                Console.WriteLine("Inserire nome alunno {0}", i + 1);
+                alunni[i] = Console.ReadLine();
+                Console.WriteLine("Mancano {0} nomi da inserire", alunni.Length- (i+1));
+            }
+            Console.WriteLine("La classe è piena");
+            Console.ReadLine();
+        }
+    }
+}
