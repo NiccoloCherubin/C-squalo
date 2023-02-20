23fa881c921e6c100c356b8d68de5e8542215810
Niccolò Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-20 16:50:25 +0100

    super push

--- LaScuolaElementare2/LaScuolaElementare2/Program.cs
+++ LaScuolaElementare2/LaScuolaElementare2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,9 +18,12 @@ namespace LaScuolaElementare
              * La maestra ha la necessità che l'elenco dei bambini che vanno in gita vada mantenuto fino alla prossima visita d'struzione
              */
             int opzione, nAlunno = 0, posizione = 0;
-            string ricerca;            
-            const int maxOpzione = 5, nAlunni = 3;
-            string[] nomeAlunni = new string[nAlunni];
+            int presentiGita = 0; // per contare quanti vengono in gita
+            string ricerca;
+            const int maxOpzione = 6, nAlunni = 3;
+            string[] nomeAlunni = new string[nAlunni]; // elenco nomi dei bambini
+            string[] gita = new string[nAlunni]; // per fare elenco di chi va in gita
+            string rispostaGita; // risposta se viene in gita o no
             //visualizzazione menù
             do
             {
@@ -32,6 +36,7 @@ namespace LaScuolaElementare
                     Console.WriteLine("[2] presenti");
                     Console.WriteLine("[3] ricerca");
                     Console.WriteLine("[4] visualizza alunno");
+                    Console.WriteLine("[5] Chi va in gita");
                     Console.WriteLine("[{0}] esci", maxOpzione);
                     opzione = Convert.ToInt32(Console.ReadLine());
                 } while (opzione < 1 || opzione > maxOpzione);
@@ -93,7 +98,7 @@ namespace LaScuolaElementare
                             // verifico se c'è il nome in elenco
                             if (trovato)
                             {
-                                Console.WriteLine("L'alunno {0} è presente", ricerca);                               
+                                Console.WriteLine("L'alunno {0} è presente", ricerca);
                             }
                             else
                             {
@@ -109,19 +114,59 @@ namespace LaScuolaElementare
                         // per vedere se ha usato ricerca                        
                         break;
                     case 4:
-                        if (posizione !=0)
+                        if (posizione != 0)
                         {
                             Console.BackgroundColor = ConsoleColor.White;
-                            Console.WriteLine($"{posizione} {nomeAlunni[posizione-1]}");
+                            Console.WriteLine($"{posizione} {nomeAlunni[posizione - 1]}");
                             Console.ReadLine();
                         }
                         else
                         {
                             Console.WriteLine("Non hai effettuato ricerca");
                         }
+                        // ripristino colori base console
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
                         Console.ReadLine();
                         break;
+                    case 5:
+                        // verifico se sono presenti degli alunni
+                        if (nAlunno != 0)
+                        {
+                            // chiedo chi va in gita
+                            for (int i = 0; i < nAlunno; i++)
+                            {
+                                Console.WriteLine("{0} viene in gita?", nomeAlunni[i]);
+                                rispostaGita = Console.ReadLine().ToLower(); // rendo la risposta minuscola per non verificare ogni possibilità
+                                // verifico risposta per dare messaggio d'errore
+                                while (rispostaGita != "sì" && rispostaGita != "no")
+                                {
+                                    Console.WriteLine("Inserire risposta valida (Sì o No)");
+                                    rispostaGita = Console.ReadLine().ToLower();
+                                }
+                                if (rispostaGita == "sì")
+                                {
+                                    gita[i] = nomeAlunni[i]; // inserisco il nome dell'alunno nell'elenco della gita
+                                    presentiGita++;
+                                }
+                            }
+                            // stampo elenco di chi viene in gita
+                            for (int i = 0; i < presentiGita; i++)
+                            {
+                                Console.WriteLine("L'alunno {0} viene in gita", gita[i]);
+                            }
+                            Console.WriteLine("premere invio per continuare");
+                            Console.ReadLine();
+                        }
+                        // messagio di errore nel caso di mancato inserimento
+                        else
+                        {
+                            Console.WriteLine("Non hai inserito nome degli alunni. Premere invio per continuare");
+                            Console.ReadLine();
+                        }
+                        break;
                 }
+
             } while (opzione != maxOpzione);     // ripete ciclo finché non esce            
         }
     }
