9e52a7280a9e12b5aecf714d79eb1831c9910a15
Niccolò Cherubin <103661034+NiccoloCehrubin@users.noreply.github.com>
2023-03-21 09:21:55 +0100

    Paint 2

---
+++ CONSOLE KEY/paint/paint2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paintsuvisualstudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            ConsoleKey tasto;
+            int riga = 0, colonna = 0;
+            int schWidht, schHeight;
+            schWidht = Console.WindowWidth;
+            schHeight = Console.WindowHeight;
+            char matita = '.';
+            bool fine = true;
+            bool ins = false;
+            Console.SetCursorPosition(riga, colonna);
+            Console.Write(matita);
+
+
+
+            do
+            {
+                Console.SetCursorPosition(0, 28);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < Console.WindowWidth - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\rX: {0} Y: {1}", colonna, riga);
+
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                if (ins)
+                {
+                    matita = ' ';
+                }
+                else
+                {
+                    matita = '.';
+                }
+                Console.SetCursorPosition(colonna, riga);
+                Console.Write(matita);
+                tasto = Console.ReadKey(true).Key;
+                switch (tasto)
+                {
+                    case ConsoleKey.Escape:
+                        fine = false;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (colonna < schWidht - 2)
+                        {
+                            colonna++;
+                        }
+
+
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (colonna >= 1)
+                        {
+                            colonna--;
+                        }
+
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (riga >= 1)
+                        {
+                            riga--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (riga < schHeight - 1)
+                        {
+                            riga++;
+                        }
+
+                        break;
+                    case ConsoleKey.Insert:
+                        if (!ins)
+                        {
+                            ins = true;
+                        }
+                        else
+                        {
+                            ins = false;
+                        }
+                        break;
+                }
+                
+
+
+            } while (fine);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
