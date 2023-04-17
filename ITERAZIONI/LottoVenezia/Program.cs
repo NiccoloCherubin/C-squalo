d80277c3c55cec7acdc3ff7a4fbce5c613361984
Niccolò Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-04-17 16:01:38 +0200

    as

--- ITERAZIONI/LottoVenezia/Program.cs
+++ ITERAZIONI/LottoVenezia/Program.cs
@@ -19,13 +19,43 @@ namespace LottoVenezia
             int estratto;
             bool esiste = false;
             int i = 0;
-            const int max = (int)Ruote.Nazionale;
-            for (int k = 0; k < max; k++)
+            int[] fortunelli = new int[5]; // numeri giocati            
+            byte scelta; // scelta della ruota
+            byte contatore = 0; // conta numeri indovinati
+            ulong puntata;
+            bool valido;
+
+            for (int k = 0; k < (int)Ruote.Nazionale; k++)
             {
-                Console.WriteLine("Ruota di {0}", (Ruote)k); // Scrivo il nome della ruota 
+                Console.WriteLine($"{k} {(Ruote)k}");
+            }
                 do
                 {
-                    estratto = random.Next(0, 91);
+                Console.WriteLine("Scegli una ruota");
+                scelta = Byte.Parse(Console.ReadLine());
+            }
+            while (scelta < 0 || scelta > (int)Ruote.Nazionale);
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Numero {0}", i + 1);
+                fortunelli[i] = Int32.Parse(Console.ReadLine());
+                if (fortunelli[i] > 0 && fortunelli[i] < 90)
+                {
+                    ++i;
+                }
+            } while (i != 5);
+            i = 0;
+            // inserimento puntata
+            Console.WriteLine("Mettere puntata");
+            valido = ulong.TryParse(Console.ReadLine(), out puntata);
+
+            for (int k = 0; k <= max; k++)
+            {
+                Console.Write(" {0,-10}: ", (Ruote)k); // Scrivo il nome della ruota 
+                do
+                {
+                    estratto = random.Next(1, 91);
                     for (int j = 0; j < i && !esiste; j++)
                     {
                         esiste = estratto == numeri[j]; // quando trova numero già presente nell'array esce dal ciclo
