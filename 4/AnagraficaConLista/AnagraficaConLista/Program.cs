194531ba5a5e19834a32aab66f2dadfeb72f2315
Niccolò Cherubin <niccolo.cherubin@ghostprison.onmicrosoft.com>
2023-10-23 18:44:26 +0200

    ripasso

---
+++ 4/AnagraficaConLista/AnagraficaConLista/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagraficaConLista
+{
+    internal class Program
+    {
+        enum Sesso
+        {
+            Maschio, Femmina,
+        }
+
+        enum StatoCivile
+        {
+            Celibe, Nubile, Coniugato, Divorziato, Separato
+        }
+
+        struct Anagrafica
+        {
+            public string nome;
+            public string cognome;
+            public DateTime dataNascita;
+            public Sesso sesso;
+            public StatoCivile statocivile;
+            public string cittadinanza;
+            public string codiceFiscale;//QIUXNG05T26Z210S
+            public override string ToString() // ridifinito metodo toString, perché comune a tutti
+            {
+                return string.Format($"Nome:{nome,-5} Cognome:{cognome,4} Data Nascita: {dataNascita.ToShortDateString(),3} Stato Civile: {statocivile,10} Sesso: {sesso,5}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Anagrafica> anagrafica = new List<Anagrafica>();
+            int ultimo = -1;
+            string[] opzioni = new string[] { "Inserimento", "Visualizza", "Cambia stato civile", "Calcolo età", "Rimozione", "Leggi log", "Tutti log", "Exit" };
+            int scelta;
+            do
+            {
+                scelta = Menu(opzioni, "COMUNE DI ROVIGO", 0, 0, ConsoleColor.DarkRed, ConsoleColor.Gray, ConsoleColor.Black);
+                Console.Clear();
+                VerifcaScelta(scelta, anagrafica, ref ultimo);
+                Console.ReadLine();
+            } while (scelta != opzioni.Length - 1);
+        }
+        static int Menu(string[] opzioni, string titolo, int x, int y, ConsoleColor coloreTitolo, ConsoleColor coloreTesto, ConsoleColor coloreSfondo)
+        {
+            int temp = y; // per salvare valore (mandatory)
+            int scelta;
+            y = temp;
+            Console.Clear();
+            Console.ForegroundColor = coloreTitolo;
+            Console.BackgroundColor = coloreSfondo;
+            Console.SetCursorPosition(x, y++); // cambio posizione al titolo
+            Console.WriteLine(titolo);
+            Console.ForegroundColor = coloreTesto;
+            for (int i = 0; i < opzioni.Length; i++)
+            {
+                Console.SetCursorPosition(x, y++);
+                Console.WriteLine($"[{i + 1}] {opzioni[i]}");
+            }
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("Inserire Opzione");
+            while (!int.TryParse(Console.ReadLine(), out scelta) || scelta < 1 || scelta > opzioni.Length) // per non fare crushare programma
+            {
+                Console.WriteLine("Inserisci qualcosa di corretto");
+            }
+            scelta--; // perché l'utente vede le opzioni aumentate di uno
+            return scelta;
+        }
+        static void VerifcaScelta(int scelta, List<Anagrafica> anagrafica, ref int ultimo)
+        {
+            string risposta;
+            int indice;
+            switch (scelta)
+            {
+                case 0:
+                    Inserimento(anagrafica, ref ultimo);
+                    break;
+                case 1:
+                    if (anagrafica.Count < 1)
+                    {
+                        anagrafica.ForEach(a=> { Console.WriteLine(a.ToString()); });
+                        Console.WriteLine("Premere invio per continuare");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Array vuoto. Fare inserimento");
+                    }
+                    break;
+                case 2:
+                    if (anagrafica.Count < 1)
+                    {
+                        Console.WriteLine($"Di chi vuoi cambiare lo stato civile? (inserisci il numero maggiore di zero e minore uguale di {ultimo})");
+                        byte numero;
+
+                        while (!Byte.TryParse(Console.ReadLine(), out numero) || numero > anagrafica.Count || numero < 0)
+                        {
+                            Console.WriteLine("Inserisci qualcosa di corretto");
+                        }
+                        CambioStatoCivile(anagrafica, numero);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Array vuoto. Premere invio per continuare");
+                        Console.ReadLine();
+                    }
+
+                    break;
+                case 3:
+                    if (anagrafica.Count < 1)
+                    {
+                        Console.WriteLine("Inserire codice fiscale della persone della quale si vuole sapere l'età");
+                        while (!ControlloStringhe(Console.ReadLine(), out risposta))
+                        {
+                            Console.WriteLine("Inserire stringa valida");
+                        }
+                        indice = anagrafica.FindIndex(a => a.nome == risposta);
+                        if (indice != -1)
+                        {
+                            Console.Write($"Età di {anagrafica[indice].nome} {anagrafica[indice].cognome}: ");
+                            Console.WriteLine(Eta(indice, anagrafica));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Persona non presente nell'archivio");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Array vuoto. Premere invio per continuare");
+
+                    }
+                    break;
+                case 4:
+                    if (anagrafica.Count < 1)
+                    {
+                        Console.WriteLine("Inserire codice fiscale della persona da eliminare");
+                        while (!ControlloStringhe(Console.ReadLine(), out risposta))
+                        {
+                            Console.WriteLine("Inserire stringa valida");
+                        }
+
+                        indice = anagrafica.FindIndex(a => a.nome == risposta);
+                        if (indice != -1)
+                        {
+                            anagrafica.RemoveAt(indice);
+                            Console.WriteLine("Eliminazione completata. Premere invio per continuare");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Elemento non presente nell'anagrafe");
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Anagrafica vuota. Fare inserimento");
+                    }
+                    break;
+
+            }
+        }
+        static void Inserimento(List<Anagrafica> anagrafica, ref int ultimo)
+        {
+            string risposta;
+            Anagrafica p1 = new Anagrafica();
+            ultimo++;
+            Console.WriteLine("Inserire nome");
+            while (!ControlloStringhe(Console.ReadLine(), out p1.nome))
+            {
+                Console.WriteLine("Inserire nome valido");
+            }
+            Console.WriteLine("Inserire cognome");
+            while (!ControlloStringhe(Console.ReadLine(), out p1.cognome))
+            {
+                Console.WriteLine("Inserire cognome valido");
+            }
+            //stato civile
+            p1.sesso = (Sesso)Menu(Enum.GetNames(typeof(Sesso)), "SESSO", 0, 0, ConsoleColor.Magenta, ConsoleColor.Gray, ConsoleColor.Black);
+            p1.statocivile = (StatoCivile)Menu(Enum.GetNames(typeof(StatoCivile)), "STATO CIVILE", 0, 0, ConsoleColor.Magenta, ConsoleColor.Gray, ConsoleColor.Black);
+            Console.Clear();
+            Console.WriteLine("inserire data di nascita");
+            while (!DateTime.TryParse(Console.ReadLine(), out p1.dataNascita))
+            {
+                Console.WriteLine("inserire data di nascita valida");
+            }
+            Console.WriteLine("Inserire codice fiscale");
+            risposta = Console.ReadLine();
+            while (!ControlloInserimento(risposta, anagrafica, ultimo))
+            {
+                Console.WriteLine("Codice fiscale già presente. Reinserire");
+                risposta = Console.ReadLine();
+            }
+            p1.codiceFiscale = risposta;
+            Console.Clear();
+
+            Console.WriteLine("Inserimento Completato. Premere invio per continuare");
+        }
+        static bool ControlloStringhe(string prova, out string stringa)
+        {
+            if (string.IsNullOrEmpty(prova))
+            {
+                stringa = "";
+                return false;
+            }
+            else
+            {
+                stringa = prova;
+                return true;
+            }
+        }
+        static void CambioStatoCivile(List<Anagrafica>anagrafe, int posizione)
+        {     
+            Anagrafica p1 = anagrafe[posizione];
+            anagrafe.Remove(p1);
+            p1.statocivile = (StatoCivile)Menu(Enum.GetNames(typeof(StatoCivile)), "STATO CIVILE", 0, 0, ConsoleColor.Magenta, ConsoleColor.Gray, ConsoleColor.Black);
+            anagrafe.Insert(posizione, p1);
+        }        
+        static bool ControlloInserimento(string codice, List<Anagrafica> codiciFiscali, int ultimo)
+        {
+            for (int i = 0; i < ultimo; i++)
+            {
+                //controllo se nell'array è presente un doppione
+                if (codice == codiciFiscali[i].codiceFiscale)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static int Eta(int indice, List<Anagrafica> anagrafe)
+        {
+            int eta = DateTime.Now.Year - anagrafe[indice].dataNascita.Year - 1; // calcolo età
+            if (DateTime.Now.Month >= anagrafe[indice].dataNascita.Month && DateTime.Now.Day > anagrafe[indice].dataNascita.Day) // verifica del mese e del giorno
+            {
+                eta++;
+            }
+            return eta;
+        }
+    }
+
+}
---
+++ 4/FusioneNucleare/FusioneNucleare/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FusioneNucleare
+{
+    internal class Program
+    {
+        static int inseriti = -1;
+        const int max = 2;
+
+        struct Studente
+        {
+            public string nome;
+            public string cognome;
+            public double voto; // per voti mezzi
+            public override string ToString()
+            {
+                return string.Format($"{nome,-15} {cognome,-5} {voto,10}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Struct
+            // array struct
+            //metodo inserimento con controllo max
+            //metodo visualizza
+            //controllo dati validi
+            //controllo doppioni
+            Studente[] classe = new Studente[max];
+            int scelta;
+            string[] opzioni = { "Inserisci", "Visualizza", "Esci" };
+            do
+            {
+                scelta = Menu(opzioni);
+                VerificaScelta(scelta, classe);
+            } while (scelta != opzioni.Length - 1);
+        }
+        static int Menu(string[] opzioni)
+        {
+            Console.Clear();
+            int scelta;
+            for (int i = 0; i < opzioni.Length; i++)
+            {
+                Console.WriteLine($" [{i + 1}] {opzioni[i]}");
+            }
+            while (ControlloScelta(out scelta, opzioni.Length))
+            {
+                Console.WriteLine("Inserisci un valore corretto");
+            }
+            scelta--;
+            return scelta; //l'utente vede le opzioni maggiorate di uno
+        }
+        static void VerificaScelta(int scelta, Studente[] studenti)
+        {
+            switch (scelta)
+            {
+                case 0:
+                    //inserimento
+                    if (inseriti <= max)
+                    {
+                        Inserimento(studenti);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Classe piena");
+                    }
+                    Console.WriteLine("Inserimento completato. Premere invio per continuare");
+                    break;
+                case 1:
+                    //visualizza
+                    if (inseriti != -1)
+                    {
+                        Visualizza(studenti);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Classe vuota. Fare inserimento");
+                    }
+                    Console.WriteLine("Premere invio per continuare");
+                    break;
+            }
+            Console.ReadLine();
+        }
+        static void Inserimento(Studente[] studenti)
+        {
+            Studente studente = new Studente(); // per non sporcare l'array
+            bool doppio;
+            inseriti++;
+            do
+            {
+
+                string prova;
+                //inserimento nome
+                Console.WriteLine($"Inserire nome dello studente {inseriti + 1}");
+                while (!ControlloStringhe(Console.ReadLine(), out prova))
+                {
+                    Console.WriteLine("Inserire un nome corretto");
+                }
+                studente.nome = prova; // assegnazione nome
+                Console.WriteLine($"Inserire cognome dello studente {inseriti + 1}");
+                //inserimento cognome
+                while (!ControlloStringhe(Console.ReadLine(), out prova))
+                {
+
+                    Console.WriteLine("Inserire un cognome corretto");
+                }
+                studente.cognome = prova; // assegnazione cognome
+                do
+                {
+                    Console.WriteLine($"Inserire voto dello studente {inseriti + 1} (0-10)");
+                } while (!double.TryParse(Console.ReadLine(), out studente.voto) || !ControlloVoto(studenti[inseriti].voto));
+
+                doppio = ControlloDoppioni(studenti, studente); // controllo doppioni
+
+                if (doppio)
+                {
+                    Console.WriteLine("Studente già inserito. Reinserire");
+                }
+            } while (doppio);
+
+            studenti[inseriti] = studente; //inserimento studente nella classe
+
+        }
+        static bool ControlloDoppioni(Studente[] studenti, Studente studente)
+        {
+            for (int i = 0; i < inseriti; i++)
+            {
+                if (studente.nome.ToLower() == studenti[i].nome.ToLower() && studente.cognome.ToLower() == studenti[i].cognome.ToLower())
+                {
+                    return true; // doppione trovato
+                }
+            }
+            return false; // doppione non trovato
+        }
+        static bool ControlloVoto(double voto)
+        {
+            if (voto < 0 || voto > 10)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static bool ControlloScelta(out int scelta, int lunghezza)
+        {
+            return !int.TryParse(Console.ReadLine(), out scelta) || scelta < 1 || scelta > lunghezza;
+        }
+        static bool ControlloStringhe(string prova, out string stringa)
+        {
+            prova = prova.Trim();
+            if (String.IsNullOrEmpty(prova))
+            {
+                stringa = null;
+                return false;
+            }
+            else
+            {
+                stringa = prova;
+                return true;
+            }
+
+        }
+        static void Visualizza(Studente[] classe)
+        {
+            for (int i = 0; i <= inseriti; i++)
+            {
+                Console.WriteLine($"STUDENTE {i + 1}");
+                Console.WriteLine(classe[i].ToString());
+                Console.WriteLine($"===================");
+            }
+        }       
+    }
+}
